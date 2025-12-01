9b4e1dbdf54b800836d10e9c5ec3b4a0815015d1
xcyelena-jpg <xcyelena@gmail.com>
2025-12-01 20:47:28 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react()],
-    base: './', // Important for GitHub Pages
+    base: './', 
     define: {
-      // Prevent crash if API_KEY is missing
       'process.env.API_KEY': JSON.stringify(env.API_KEY || ''),
     },
     build: {
